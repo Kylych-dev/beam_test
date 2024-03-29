7ce36c94f912cb50e1c6805a97ea31fff894c64e
kylych-dev <mirbekov1kylych@gmail.com>
2024-03-29 18:30:27 +0600

    .

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -102,7 +102,7 @@ USE_TZ = True
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
