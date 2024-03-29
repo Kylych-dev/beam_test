79d1610062caf28daa83550ff842605348323181
kylych-dev <mirbekov1kylych@gmail.com>
2024-03-29 16:27:31 +0600

    deploy

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -25,15 +25,17 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     'django_filters',
     'drf_yasg',
+    'channels',
 
     # apps
     'apps.product',
     'apps.store',
-    'apps.accounts'
+    'apps.accounts',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -100,6 +102,8 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from .yasg import urlpatterns as doc_ts
+from apps.accounts.consumers import NotificationConsumer
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,5 +13,11 @@ urlpatterns = [
 ]
 urlpatterns += doc_ts
 
+websocket_urlpatterns = [
+    path("ws/notifications/", NotificationConsumer.as_asgi())
+]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
